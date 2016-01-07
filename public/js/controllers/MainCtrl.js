eedcf1cfd79dfbbc89f7e0073e458296fba25747
Jeff Dunn <jdunn522@gmail.com>
2016-01-07 10:40:50 -0800

    Create Angular controllers

---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,6 @@
+/* globals angular */
+
+angular.module('MainCtrl', [])
+  .controller('MainController', ['$scope', function ($scope) {
+    $scope.tagLine = 'To the moon and back!'
+  }])
---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,6 @@
+/* globals angular */
+
+angular.module('NerdCtrl', [])
+  .controller('NerdController', ['$scope', function ($scope) {
+    $scope.tagLine = 'Nothing beats a pocket protector!'
+  }])
